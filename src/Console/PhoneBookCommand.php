02b38cf1e6f3158f5f811b634c848daabef0c4e8
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2020-04-18 12:24:19 +0300

    added ponebook:fix command

--- src/Console/PhoneBookCommand.php
+++ src/Console/PhoneBookCommand.php
@@ -13,7 +13,7 @@ class PhoneBookCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'phonebook:fix';
+    protected $signature = 'phonebook {--fix}';
 
     /**
      * The console command description.
@@ -39,52 +39,61 @@ class PhoneBookCommand extends Command
      */
     public function handle()
     {
-        $start = new \DateTime();
-        $this->comment('[ '.$start->format('d-m-Y H:i:s').' ] Started cleaning contact names');
+        if($this->option('fix')){
+            $start = new \DateTime();
+            $this->comment('[ '.$start->format('d-m-Y H:i:s').' ] Started cleaning contact names');
 
-        $contacts = Contact::all();
-        $count = false;
-        $counter = 0;
+            $contacts = Contact::all();
+            $count = false;
+            $counter = 0;
 
-        $bar = $this->output->createProgressBar(count($contacts));
+            $bar = $this->output->createProgressBar(count($contacts));
 
-        foreach ($contacts as $key => $contact) {
-            $display_name = explode(" ",$contact->display_name);
-            foreach ($display_name as $key => $value) {
-                $display_name[$key] = trim($value);
-            }
-            $first_name = trim($contact->first_name);
-            $last_name = trim($contact->last_name);
-
-            if (!empty($first_name) && !empty($display_name[1])) {
-                if (strlen($first_name) > strlen($display_name[1])) {
-                    $display_name[1] = $first_name;
-                }elseif(strlen($display_name[1]) > strlen($first_name)){
-                    $first_name = $display_name[1];
+            foreach ($contacts as $key => $contact) {
+                $display_name = explode(" ",$contact->display_name);
+                foreach ($display_name as $key => $value) {
+                    $display_name[$key] = trim($value);
                 }
-            }
+                $first_name = trim($contact->first_name);
+                $last_name = trim($contact->last_name);
 
-            if (!empty($last_name) && !empty($display_name[0])) {
-                if (strlen($last_name) > strlen($display_name[0])) {
-                    $display_name[0] = $last_name;
-                }elseif(strlen($display_name[0]) > strlen($last_name)){
-                    $last_name = $display_name[0];
+                if (!empty($first_name) && !empty($display_name[1])) {
+                    if (strlen($first_name) > strlen($display_name[1])) {
+                        $display_name[1] = $first_name;
+                    }elseif(strlen($display_name[1]) > strlen($first_name)){
+                        $first_name = $display_name[1];
+                    }
+                    $count = true
                 }
+
+                if (!empty($last_name) && !empty($display_name[0])) {
+                    if (strlen($last_name) > strlen($display_name[0])) {
+                        $display_name[0] = $last_name;
+                    }elseif(strlen($display_name[0]) > strlen($last_name)){
+                        $last_name = $display_name[0];
+                    }
+                    $count = true
+                }
+                
+                $contact->first_name = $first_name;
+                $contact->last_name = $last_name;
+                $contact->display_name = implode(" ",$display_name);
+
+                $contact->save();
+                
+                if($count){
+                    $counter++;
+                    $count = false;
+                }
+
+                $bar->advance();
             }
+            $bar->finish();
 
-            $contact->first_name = $first_name;
-            $contact->last_name = $last_name;
-            $contact->display_name = implode(" ",$display_name);
+            $stop = new \DateTime();
+            $duration = $stop->diff($start);
 
-            $contact->save();
-
-            $bar->advance();
+            $this->comment('[ '.$stop->format('d-m-Y H:i:s').' ] Finished cleaning '.$count.'contact names in '.$duration->format('%I Minute(s) %S Second(s)'));
         }
-        $bar->finish();
-
-        $stop = new \DateTime();
-        $duration = $stop->diff($start);
-
-        $this->comment('[ '.$stop->format('d-m-Y H:i:s').' ] Finished cleaning '.$count.'contact names in '.$duration->format('%I Minute(s) %S Second(s)'));
     }
 }
